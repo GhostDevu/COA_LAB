acc9b2e419a7b956cb77afc33e44459a83cb731a
GhostDevanshu <dev_iitkgp2022@kgpian.iitkgp.ac.in>
2024-11-10 10:10:50 +0530

    non_restoring

--- non_restoring_division.asm
+++ non_restoring_division.asm
@@ -5,13 +5,15 @@ slli $5,$5,6
 slli $4,$4,1 //loop
 bmi $4,2
 sub $4,$4,$5
-br 2
+br 1
 add $4,$4,$5 // add_loop
+bmi $4,1     // after_add_sub
 addi $4,$4,1
-subi $1,$1,1 // after_add_sub
+subi $1,$1,1 // skip_Q_1
 bz $1,1
-br -9
+br -10
 bpl $4,1 // end_loop
 add $4,$4,$5
 andi $2,$4,63 // skip_correction
 andi $3,$4,4032
+halt
--- processed_codes/processed_code_non_restoring_division.asm
+++ processed_codes/processed_code_non_restoring_division.asm
@@ -5,13 +5,15 @@ slli $5,$5,6
 slli $4,$4,1
 bmi $4,2
 sub $4,$4,$5
-br 2
+br 1
 add $4,$4,$5
+bmi $4,1
 addi $4,$4,1
 subi $1,$1,1
 bz $1,1
-br -9
+br -10
 bpl $4,1
 add $4,$4,$5
 andi $2,$4,63
 andi $3,$4,4032
+halt
