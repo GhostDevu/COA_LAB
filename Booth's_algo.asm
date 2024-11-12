f45032066f5be0c7d43bf6c7b7004ae2acfeb43e
GhostDevanshu <dev_iitkgp2022@kgpian.iitkgp.ac.in>
2024-11-12 15:54:17 +0530

    booths try neg

--- Booth's_algo.asm
+++ Booth's_algo.asm
@@ -1,6 +1,5 @@
-addi $11,$0,0
-ld $1,0($11)
-ld $2,1($11)
+subi $1,$0,2
+subi $2,$0,-3
 addi $3, $0, 32       // Bit width
 move $10, $3          // Counter for loop (no_of_bits)
 addi $11, $3, -1      // no_of_bits - 1 for shift
